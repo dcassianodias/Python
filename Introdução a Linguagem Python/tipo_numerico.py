bd75d40076daf89854b4f53884290df1f0b24907
Danilo Cassiano Dias <59808307+dcassianodias@users.noreply.github.com>
2020-12-08 21:56:44 -0300

    Add files via upload

---
+++ Introdução a Linguagem Python/tipo_numerico.py
@@ -0,0 +1,17 @@
+"""
+Tipo númerico
+
+num = 4
+
+int(5/2)
+5 // 2 (arredonda a divisão)
+3 ** 3 (elevado a 3)
+1_000_000 (pode separar os números com underline)
+num+= (mesma coisa que num = num + 1)
+num-=
+num*=
+num/=
+type(num) = retorna o tipo da variável
+type(4)
+"""
+
